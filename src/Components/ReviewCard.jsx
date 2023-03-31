66d64eff3fc27aac7d9e2572b4855ce78f599311
Piotr-Szyszkowski <peter.szyszkowski@protonmail.com>
2023-03-31 17:47:08 +0100

    happy with initial look of review card

--- src/Components/ReviewCard.jsx
+++ src/Components/ReviewCard.jsx
@@ -1,9 +1,35 @@
-function ReviewCard(reviewObj) {
+import "../Css/ReviewCard.css";
+
+function ReviewCard({
+  title,
+  category,
+  release_date,
+  cover_img,
+  review_intro,
+}) {
   //   const { title, category, release_date, cover_img, review_intro } = reviewObj;
-  console.log(reviewObj);
+  // console.log(reviewObj);
   return (
-    <section>
-      <h1>Title: {reviewObj.reviewObj.title}</h1>
+    <section className="ReviewCard">
+      <div className="upper">
+        <div className="upper-left">
+          <h1 className="title">{title}</h1>
+          <div>
+            <h2 className="category">{category}</h2>
+            <h2 className="release_date">{release_date}</h2>
+          </div>
+        </div>
+        <div className="cover_image-container">
+          <img
+            className="cover_image"
+            alt={`Cover for ${title}`}
+            src={cover_img}
+          />
+        </div>
+      </div>
+      <div className="lower">
+        <p className="review_intro">{review_intro}</p>
+      </div>
     </section>
   );
 }
--- src/Components/Reviews.jsx
+++ src/Components/Reviews.jsx
@@ -1,22 +1,42 @@
 import { useEffect, useState } from "react";
 import { getAllReviews } from "../Utils/api";
 import ReviewCard from "./ReviewCard";
+import "../Css/Reviews.css";
 
 function Reviews() {
   const [reviewsList, setReviewsList] = useState([]);
 
   useEffect(() => {
     getAllReviews().then((reviewsFromApi) => {
+      console.log(reviewsFromApi);
       setReviewsList(reviewsFromApi);
     });
   }, []);
 
-  // console.log(reviewsList);
+  console.log(reviewsList);
   return (
-    <section>
-      {reviewsList.map((reviewObj) => {
-        return <ReviewCard key={reviewObj.review_id} reviewObj={reviewObj} />;
-      })}
+    <section className="Reviews">
+      {reviewsList.map(
+        ({
+          review_id,
+          title,
+          category,
+          release_date,
+          cover_img,
+          review_intro,
+        }) => {
+          return (
+            <ReviewCard
+              key={review_id}
+              title={title}
+              category={category}
+              release_date={release_date}
+              cover_img={cover_img}
+              review_intro={review_intro}
+            />
+          );
+        }
+      )}
     </section>
   );
 }
