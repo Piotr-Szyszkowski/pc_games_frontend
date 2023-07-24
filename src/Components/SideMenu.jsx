52bdf883a4607ecf79888da6add404ebbab35506
Piotr-Szyszkowski <peter.szyszkowski@protonmail.com>
2023-07-24 17:14:07 +0100

    added overlay to side manu and x button to close

--- src/Components/SideMenu.jsx
+++ src/Components/SideMenu.jsx
@@ -7,13 +7,24 @@ function SideMenu({ sideMenuProps }) {
   console.log(toggleMenu, isMenuOpen);
   return (
     <div className="SideMenu-Container">
-      {/* <button className="placeholder-button" onClick={toggleMenu}>
-        Open Menu
-      </button> */}
+      {isMenuOpen && <div className="overlay"></div>}
       <div className={`SideMenu-Content-Wrapper ${isMenuOpen ? "open" : ""}`}>
-        <button className="placeholder-button" onClick={toggleMenu}>
-          Close Menu
-        </button>
+        <div
+          className={`SideMenu-CloseButton-Wrapper ${
+            !isMenuOpen ? "closed" : ""
+          }`}
+        >
+          <svg
+            className={`SideMenu-CloseButton `}
+            onClick={toggleMenu}
+            viewBox="0 0 100 100"
+            width="16"
+            height="16"
+            fill="none"
+          >
+            <path d="M68.71,35.75a4.69,4.69,0,0,0-8-3.12L50,43.37,39.26,32.63a4.69,4.69,0,0,0-6.63,6.63L43.37,50,32.63,60.74a4.69,4.69,0,0,0,6.63,6.63L50,56.63,60.75,67.37a4.69,4.69,0,0,0,6.63-6.63L56.63,50,67.37,39.26A4.63,4.63,0,0,0,68.71,35.75ZM50,0a50,50,0,1,0,50,50A50,50,0,0,0,50,0Zm0,90.63A40.63,40.63,0,1,1,90.63,50,40.67,40.67,0,0,1,50,90.63Z"></path>
+          </svg>
+        </div>
       </div>
     </div>
   );
