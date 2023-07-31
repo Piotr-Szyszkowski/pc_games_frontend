910faf523017a8a1522bd300c934230dc6c6b991
Piotr-Szyszkowski <peter.szyszkowski@protonmail.com>
2023-07-31 17:39:51 +0100

    tackled some space waste

--- src/Components/ReviewCard.jsx
+++ src/Components/ReviewCard.jsx
@@ -11,24 +11,26 @@ function ReviewCard({
   // console.log(reviewObj);
   return (
     <section className="ReviewCard">
-      <div className="upper">
-        <div className="upper-left">
-          <h1 className="title">{title}</h1>
-          <div>
-            <h2 className="category">{category}</h2>
-            <h2 className="release_date">{release_date}</h2>
-          </div>
-        </div>
-        <div className="cover_image-container">
-          <img
-            className="cover_image"
-            alt={`Cover for ${title}`}
-            src={cover_img}
-          />
-        </div>
+      <div className="cover_image-container">
+        <img
+          className="cover_image"
+          alt={`Cover for ${title}`}
+          src={cover_img}
+        />
+        {/* </div> */}
       </div>
-      <div className="lower">
+      {/* <div className="upper"> */}
+      {/* <div className="upper-left"> */}
+      <div className="content">
+        <h1 className="title">{title}</h1>
+        <div>
+          <h2 className="category">{category}</h2>
+          <h2 className="release_date">{release_date}</h2>
+        </div>
+        {/* </div> */}
+        {/* <div className="lower"> */}
         <p className="review_intro">{review_intro}</p>
+        {/* </div> */}
       </div>
     </section>
   );
